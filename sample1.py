4ed3a6cb373ebc388aed74783c74cf3f11a4b23d
FRC4564 <steve.jacobs@iname.com>
2013-09-05 18:39:43 -0400

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -6,37 +6,43 @@ import scrnio    #copy to /usr/lib/python2.7
 
 
 try:
+    # Initialize the screen
     s = scrnio.screen()
+    if s.WIDTH < 80 or s.HEIGHT<25:
+    	raise ValueError("Expand console to at least 25 rows by 80 columns")
+    # Once color is set, screen dispaly commands and form controls will default to this color
     s.setcolor(s.YELLOW,s.BLACK,s.BOLD)
+    # Clear the screen
     s.clear()
+
     s.setcolor(s.BLUE,s.YELLOW)   
     s.textat(1,1,'SCRNIO Library Sample')
 
-    #Input form
+    #Setup a form with input controls
     fin = scrnio.form(s)
     s.setcolor(s.CYAN,s.BLUE,s.BOLD)
     fin.addHBar("Steering",3,3,40,1200,2400,value=2000,delta = 10,type='POINT')
     fin.addHBar("Angular velocity",7,3,length=35,min=0,max=0.3,value=.05,delta=.01)
-
+    # These controls will allow dynamic adjustment of the color of the 'Seconds' output control
     fin.addHBar("FG",3,50,length=8,min=0,max=7,value=7,delta=1)
     fin.addHBar("BG",7,50,length=8,min=0,max=7,value=0,delta=1)
 
     s.setcolor(s.CYAN,s.RED,s.BOLD)
     fin.addCheckbox("ckbEnable",3,65,"Enable Robot")
     fin.addButton("btnExit",8,65,"Exit",width=10)
+    
+    # Set the Steering control as being selected (active for input)
     fin.select("Steering")
     fin.show()
 
-    
-    
-    # Output form
+    # Setup a second form for output controls
     fout = scrnio.form(s)
     s.setcolor(s.WHITE,s.MAGENTA,s.BOLD)
     fout.addHBar("Seconds",19,11,length=60,min=0,max=59,value=0)
     fout.addHBar("Sine",22,11,length=60,min=-180,max=180,value=0,type='POINT')
     fout.show()
 
-    # Text box    
+    # Set a box to show keystrokes in    
     s.setcolor(s.WHITE,s.MAGENTA,s.BOLD)
     s.box(13,5,5,75,fill=True,title='Keystrokes',footer='All common keys decoded')
     text = ''
